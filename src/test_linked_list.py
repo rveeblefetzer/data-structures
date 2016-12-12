dc787c4a3a2ec0f8763cc443628df90fa0280424
Marc Fieser <midfies@gmail.com>
2016-12-12 13:33:51 -0800

    added tests for linked_list init

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -8,8 +8,30 @@ def test_node_init_data():
     assert temp.data == 2
 
 
-def test_node_init_next():
-    """Test that Node constructor works."""
+def test_node_init_next_none():
+    """Test that Node constructor works with empty next."""
     from linked_list import Node
     temp = Node(2)
-    assert temp.next == None
+    assert temp.next is None
+
+
+def test_node_init_next_node():
+    """Test that Node constructor works with non empty next."""
+    from linked_list import Node
+    temp = Node(2)
+    temp2 = Node(1, temp)
+    assert temp2.next == temp
+
+
+def test_linked_list_init_none():
+    """Test that link_list initializes empty."""
+    from linked_list import LinkedList
+    temp = LinkedList
+    assert temp.head is None
+
+def test_linked_list_init_none():
+    """Test that link_list initializes empty."""
+    from linked_list import LinkedList, Node
+    node = node(2)
+    temp = LinkedList(node)
+    assert temp.head == node
