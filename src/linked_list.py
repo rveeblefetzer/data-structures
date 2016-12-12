b7379d20630f5b4c7887cfbc896532f41cd94ac6
Rick Valenzuela <devnull@rickv.com>
2016-12-12 13:32:01 -0800

    Add LinkedList class def

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,10 +1,18 @@
-"""Something."""
+"""Create a class for linked lists and a few methods to manipulate it."""
 
 
 class Node(object):
-    """Something."""
+    """Create class to represent individual elements in data structure."""
 
     def __init__(self, data, next=None):
         """Something."""
         self.data = data
         self.next = next
+
+
+class LinkedList(object):
+    """Create linked lists."""
+
+    def __init__(self, head=None):
+        """Instantiate objects with a head defaulted to None."""
+        self.head = head
