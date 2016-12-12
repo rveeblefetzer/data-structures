6779839d59f1bb820a98849cdcad3f1ae5342ba0
Rick Valenzuela <devnull@rickv.com>
2016-12-12 13:11:55 -0800

    Added Node class

---
+++ src/linked_list.py
@@ -0,0 +1,10 @@
+"""Something."""
+
+
+class Node(object):
+    """Something."""
+
+    def __init__(self, data, next=None):
+        """Something."""
+        self.data = data
+        self.next = next
