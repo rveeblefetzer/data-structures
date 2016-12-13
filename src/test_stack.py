05c86d17a574abf2cfc299543ff33766cc86fc34
Rick Valenzuela <devnull@rickv.com>
2016-12-13 13:44:51 -0800

    added push test on linked list w iterables

--- src/test_stack.py
+++ src/test_stack.py
@@ -2,6 +2,12 @@
 
 import pytest
 
+ITER_TABLE = [
+    ['string', "('g', 'n', 'i', 'r', 't', 's')"],
+    [[1, 2, 3], "(3, 2, 1)"],
+    [(1, 2, 3), "(3, 2, 1)"],
+]
+
 
 @pytest.fixture
 def test_stack():
@@ -20,3 +26,17 @@ def test_push_to_empty_stack(test_stack):
     """Test that push works on empty stack."""
     test_stack.push('phone')
     assert test_stack._stack.head.data == 'phone'
+
+
+def test_push_to_non_empty_stack(test_stack):
+    """Test pushing to populated stack."""
+    test_stack.push(4)
+    test_stack.push(5)
+    assert test_stack._stack.head.next.data == 4
+
+
+@pytest.mark.parametrize("iterable, result", ITER_TABLE)
+def test_init_stack_w_iterables(iterable, result):
+    from stack import Stack
+    test_stack = Stack(iterable)
+    assert test_stack._stack.display == result
