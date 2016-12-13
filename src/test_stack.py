86010b6edbdf39b6f1fcd088abefa5b70c524a95
Rick Valenzuela <devnull@rickv.com>
2016-12-13 13:34:00 -0800

    test pushing to empty stack

--- src/test_stack.py
+++ src/test_stack.py
@@ -14,3 +14,9 @@ def test_stack():
 def test_init_stack(test_stack):
     """Test that stack is initialized."""
     assert test_stack._stack.head is None
+
+
+def test_push_to_empty_stack(test_stack):
+    """Test that push works on empty stack."""
+    test_stack.push('phone')
+    assert test_stack._stack.head.data == 'phone'
