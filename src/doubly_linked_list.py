221401e7139001c96a1b08d267abfbf24fd21877
Rick Valenzuela <devnull@rickv.com>
2016-12-14 12:26:55 -0800

    add Dll obj def

--- src/doubly_linked_list.py
+++ src/doubly_linked_list.py
@@ -3,8 +3,9 @@
 
 
 class Node(object):
-    """Implementation of a Node for a doubly-linked list."""
-        def __init__(self, data, after=None, before=None):
+    u"""Implementation of a Node for a doubly-linked list."""
+
+    def __init__(self, data=None, after=None, before=None):
         """Initialize Node instance."""
         self.data = data
         self.after = after
@@ -12,7 +13,7 @@ class Node(object):
 
 
 class Dll(object):
-    """Implementation for a doubly-linked-list.
+    u"""Implementation for a doubly-linked-list.
 
     Methods :
 
@@ -29,19 +30,22 @@ class Dll(object):
         Will remove the last value from the tail of the list and return it.
 
     remove(val)  -
-        Will remove the first instance of ‘val’ found in the list, starting from the head. If ‘val’ is not present, it will raise an appropriate Python exception.
+        Will remove the first instance of ‘val’ found in the list,
+        starting from the head. If ‘val’ is not present, it will raise
+        an appropriate Python exception.
     """
 
-    def __init__(self):
-        """Initialize a doubly-linked-list."""
-        pass
+    def __init__(self, head, tail):
+        u"""Initialize a doubly-linked-list."""
+        self.head = head
+        self.tail = tail
 
     def push(self, val):
-        """Will insert the value ‘val’ at the head of the list."""
+        u"""Will insert the value ‘val’ at the head of the list."""
         pass
 
     def append(self, val):
-        """Will append the value ‘val’ at the tail of the list."""
+        u"""Will append the value ‘val’ at the tail of the list."""
         pass
 
     def pop(self):
@@ -53,5 +57,5 @@ class Dll(object):
         pass
 
     def remove(self, val):
-        """Will remove the first instance of ‘val’ found in the list."""
+        u"""Will remove the first instance of ‘val’ found in the list."""
         pass
