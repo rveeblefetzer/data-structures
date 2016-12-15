79f8275c5f930f1e17e191a2a5f5a74d6dfc2dfb
Rick Valenzuela <devnull@rickv.com>
2016-12-15 12:43:15 -0800

    Add tests for empty queue

--- src/test_queue.py
+++ src/test_queue.py
@@ -1,2 +1,45 @@
 """Testing for Queue Class."""
 
+import pytest
+
+
+@pytest.fixture
+def empty_queue():
+    """Fixture for empty queue."""
+    from queue import Queue
+    queue = Queue()
+    return queue
+
+
+@pytest.fixture
+def one_elem_queue(empty_queue):
+    """Fixture for single node queue."""
+    empty_queue.enqueue('Seattle')
+    return empty_queue
+
+
+@pytest.fixture
+def two_elem_queue(one_elem_queue):
+    """Fixture for two-node, populated dll."""
+    one_elem_queue.enqueue(5)
+    return one_elem_queue
+
+
+@pytest.fixture
+def three_elem_queue(two_elem_queue):
+    """Fixture for three-node queue."""
+    two_elem_queue.enqueue(7)
+    return two_elem_queue
+
+
+# node tests
+
+def test_queue_exists_and_empty(empty_queue):
+    """Test that node data is none after initialization."""
+    assert empty_queue._queue.head.data is None
+
+
+def test_queue_has_no_head(empty_queue):
+    """Test that initialized queue has no head."""
+    empty_queue.enqueue("Seattle")
+    assert empty_queue._queue.head.data == "Seattle"
